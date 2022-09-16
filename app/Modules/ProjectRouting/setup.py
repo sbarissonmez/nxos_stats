28e093d14400e926dfbe973be842c7acfb9eae4e
Baris <s.barissonmez@gmail.com>
2022-09-16 21:44:33 +0300

    app/modules

---
+++ app/Modules/ProjectRouting/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ProjectRouting",
+    version="1.0.0",
+    author="Chris Oberdalhoff",
+    author_email="",
+    description="Perform operations on network prefix-list",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    install_requires=("ipaddress", "sqlite3", "collections", "netmiko", "time", "os", "openpyxl"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
